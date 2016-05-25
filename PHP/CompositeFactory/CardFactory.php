58800c5b59bf11e7324eb1ea9471e986a61dd7f0
Mark Berube <mark@myfonts.com>
2016-05-25 15:03:21 -0400

    adding Composite Card Factory problem

---
+++ PHP/CompositeFactory/CardFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'Card.php';
+
+class CardFactory
+{
+	public static $values = ['Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace'];
+
+	public static function createCardDeck()
+	{
+
+	}
+}
