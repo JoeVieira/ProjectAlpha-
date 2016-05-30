dae8ec8ab5f9b90c6f0345ee75dfb102c3d984f0
MFoster <mattfoster01@gmail.com>
2016-05-30 18:23:00 -0400

    Satisfies requirements for JS specifications

--- JS/PrioritizedQueue.js
+++ JS/PrioritizedQueue.js
@@ -6,7 +6,8 @@
 
  Problem statement
  =================
- Implement a Priority Queue. This should function like a normal queue, but allowing for retrieving the top priority item, rather than the first in.
+ Implement a Priority Queue. This should function like a normal queue, 
+ but allowing for retrieving the top priority item, rather than the first in.
 
  - Assume that features of the most recent language standard are available.
  - We expect good performance.
@@ -30,4 +31,42 @@ Example Usage:
 Example output:
  {thing: "value2"}
 */
+
+
+function PriorityQueue(){
+	this.queue = [];
+}
+
+PriorityQueue.prototype.enq = function(data, priority){
+	this.queue.push({ data: data, priority: priority});
+	this.sort();
+}
+
+PriorityQueue.prototype.sort = function(){
+	this.queue.sort(this.sortCompare);
+}
+
+PriorityQueue.prototype.sortCompare = function(a, b){
+	if(a.priority > b.priority){
+		return 1;
+	} else if(a.priority < b.priority){
+		return -1;
+	} else {
+		return 0;
+	}
+}
+
+PriorityQueue.prototype.deq = function(){
+	var obj = this.queue.pop();
+	return obj.data;
+}
+
+ var queue = new PriorityQueue();
+
+ queue.enq({ name: 'charlie'}, 0);
+ queue.enq({ name: 'delta'}, 9);
+ queue.enq({ name: 'alpha'}, 5);
+
+ var data = queue.deq();
+ console.log(data);
 
