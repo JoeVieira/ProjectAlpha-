6a5cc3afe973c130c99fd51f92a64c6fc3c019e7
Jonathan Cyr <jcyr@clarku.edu>
2017-04-24 19:04:25 -0400

    Finished array flatten, add more test cases later on

--- PHP/Arrays/ArrayFlatten.php
+++ PHP/Arrays/ArrayFlatten.php
@@ -17,10 +17,35 @@
  *
  */
 
+// This function assumes that the given array will only 
+// contain two variable types: array and integer
+$output_array = array();
+
 function flattenArray($array) {
 
+	global $output_array;
+
+	foreach($array as $element) {
+    if (gettype($element) == "integer") {
+    	$output_array[] = $element;
+
+    } elseif (gettype($element) == "array") {
+    	flattenArray($element);
+    }
+	}
+  return $output_array;
 }
 
-$array = [1,2,3,[4,5,6],[7,[8,9]]];
+$array_original = [1,2,3,[4,5,6],[7,[8,9]]];
+$array_1        = [1,2,3,[4,[5]],[6,[7,[8]]]];
 
-print flattenArray($array);
+$test_cases = [$array_original, $array_1];
+
+for ($i = 0; $i < count($test_cases); $i++) {
+	print("Here is test case #{$i}:");
+	print_r($test_cases[$i]);
+
+  print("Here is the answer for test case #{$i}");
+  print_r(flattenArray($test_cases[$i]));
+  $output_array = array();
+}
